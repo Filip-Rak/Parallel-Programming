378668610205f26f6a2cbab0aa5ec5d25a35608b
Filip-Rak <Filip.wp.pl2014@wp.pl>
2024-12-17 19:25:55 +0100

    add some other exercises I suppouse

--- lab_10/pde/openmp_zaleznosci.c
+++ lab_10/pde/openmp_zaleznosci.c
@@ -5,40 +5,46 @@
 
 #define N 1000000
 
-int main(){
+int main()
+{
 
-  int i;
-  double* A = malloc((N+2)*sizeof(double));
-  double* B = malloc((N+2)*sizeof(double));
-  double suma;
+    /* Alokacja i Inicjalizacja */
+    double* A = malloc((N+2)*sizeof(double));
+    double* B = malloc((N+2)*sizeof(double));
 
-  for(i=0;i<N+2;i++) A[i] = (double)i/N;
-  for(i=0;i<N+2;i++) B[i] = 1.0 - (double)i/N;
+    for(int i=0;i<N+2;i++) 
+        A[i] = (double)i/N;
 
-  double t1 = omp_get_wtime();
-  for(i=0; i<N; i++){
-    A[i] += A[i+2] + sin(B[i]);
-  }
-  t1 = omp_get_wtime() - t1;
+    for(int i=0;i<N+2;i++) 
+        B[i] = 1.0 - (double)i/N;
 
-  suma = 0.0;
-  for(i=0;i<N+2;i++) suma+=A[i];
-  printf("suma %lf, czas obliczen %lf\n", suma, t1);
+    double t1 = omp_get_wtime();
+    for(int i=0; i<N; i++)
+        A[i] += A[i + 2] + sin(B[i]);
 
+    t1 = omp_get_wtime() - t1;
 
+    double suma; = 0.0;
+    for(int i=0;i<N+2;i++) 
+        suma+=A[i];
+    
+    printf("suma %lf, czas obliczen %lf\n", suma, t1);
 
-  for(i=0;i<N+2;i++) A[i] = (double)i/N;
-  for(i=0;i<N+2;i++) B[i] = 1.0 - (double)i/N;
+    /* Inicjalizacja danych */
+    for(int i=0;i<N+2;i++) 
+        A[i] = (double)i/N;
+    for(int i=0;i<N+2;i++) 
+        B[i] = 1.0 - (double)i/N;
 
-  t1 = omp_get_wtime();
+    t1 = omp_get_wtime();
 
-  // wersja równoległa
+    /* Wersja Rownolegla */
 
-  t1 = omp_get_wtime() - t1;
+    t1 = omp_get_wtime() - t1;
 
-  suma = 0.0;
-  for(i=0;i<N+2;i++) suma+=A[i];
-  printf("suma %lf, czas obliczen rownoleglych %lf\n", suma, t1);
+    suma = 0.0;
+    for(i=0;i<N+2;i++) suma+=A[i];
+    printf("suma %lf, czas obliczen rownoleglych %lf\n", suma, t1);
 
 
 }
