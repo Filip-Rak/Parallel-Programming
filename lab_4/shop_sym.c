9a394cc3602ec3d48d79ee5f2ffacaa65d3fb517
Filip-Rak <Filip.wp.pl2014@wp.pl>
2025-02-17 15:25:09 +0100

    change lore

--- lab_4/shop_sym.c
+++ lab_4/shop_sym.c
@@ -1,12 +1,12 @@
-// -------------------------------------------------------------
-// ---------------------- Exercise -----------------------------
-// Simulate a pub with clients being threads.-------------------
-// With unique limited beer mugs and unique limited beer taps.--
-// Use trylock and busy-waiting to show how much work a thread--
-// could have done in the meantime. ----------------------------
-// -------------------------------------------------------------
-// Execution arguments: (name), client_number, mug_number ------
-// -------------------------------------------------------------
+// ------------------------------------------------------------------------
+// ---------------------- Exercise ----------------------------------------
+// Simulate a coffe shop with clients being threads.-----------------------
+// With unique limited number of mugs and unique limited coffee machines.--
+// Use trylock and busy-waiting to show how much work a thread-------------
+// could have done in the meantime. ---------------------------------------
+// ------------------------------------------------------------------------
+// Execution arguments: (name), client_number, mug_number -----------------
+// ------------------------------------------------------------------------
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -21,8 +21,8 @@
 
 // Consts
 #define ARG_NUMBER 3
-#define TAP_NUMBER 4
-#define DRINKING_LIMIT_MAX 3
+#define COFFE_MACHINE_NUMBER 4
+#define DRINKING_LIMIT_MAX 5
 #define TAP_NAME_LENGTH 50
 #define MUG_FILL_TIME 25
 #define MUG_DRINK_TIME 50
@@ -33,18 +33,18 @@ typedef struct
     char name[TAP_NAME_LENGTH];
     int fills;
     pthread_mutex_t mutex;
-} Beer_Tap;
+} Coffe_Machine;
 
 typedef struct 
 {
     int picks;
     pthread_mutex_t mutex;
-} Beer_Mug;
+} Coffe_Mug;
 
-Beer_Tap beer_taps[TAP_NUMBER] = {{"Perla", 0, PTHREAD_MUTEX_INITIALIZER}, {"Zywiec", 0, PTHREAD_MUTEX_INITIALIZER}, 
-    {"Heineken", 0, PTHREAD_MUTEX_INITIALIZER}, {"Water (96%)", 0, PTHREAD_MUTEX_INITIALIZER}};
+Coffe_Machine coffe_machines[COFFE_MACHINE_NUMBER] = {{"Espresso", 0, PTHREAD_MUTEX_INITIALIZER}, {"Cappucino", 0, PTHREAD_MUTEX_INITIALIZER}, 
+    {"Mocha", 0, PTHREAD_MUTEX_INITIALIZER}, {"Water (96%)", 0, PTHREAD_MUTEX_INITIALIZER}};
 
-Beer_Mug* beer_mug;
+Coffe_Mug* coffe_mug;
 int mug_number;
 
 //--------------------
@@ -55,9 +55,8 @@ void* thread_func(void* arg);
 
 // Thread helper functions
 int wait_for_a_mug(int* done_work);
-int wait_for_a_tap(int* done_work);
+int wait_for_a_machine(int* done_work);
 void wait_to_return_tap(int used_tap_index, int* done_work);
-void wait_to_return_mug(int used_mug_index, int* done_work);
 
 // Main function
 int main(int argc, char* argv[])
@@ -81,7 +80,7 @@ int main(int argc, char* argv[])
     }
 
     // Allocate dynamic memory
-    beer_mug = malloc(mug_number * sizeof(Beer_Mug));
+    coffe_mug = malloc(mug_number * sizeof(Coffe_Mug));
 
     pthread_t *threads = malloc(client_number * sizeof(pthread_t));
     bool* thread_created = malloc(client_number * sizeof(bool));
@@ -89,12 +88,12 @@ int main(int argc, char* argv[])
     int *done_work = malloc(client_number * sizeof(int));
 
     // Verify if memory allocation was successful
-    if(beer_mug == NULL || threads == NULL || thread_created == NULL || thread_ids == NULL || done_work == NULL)
+    if(coffe_mug == NULL || threads == NULL || thread_created == NULL || thread_ids == NULL || done_work == NULL)
     {
         printf("ERROR: Failed to allocate memory! Exitting...\n");
 
         // Clear dynamically allocated memory
-        free(beer_mug);
+        free(coffe_mug);
         free(threads);
         free(thread_created);
         free(thread_ids);
@@ -106,8 +105,8 @@ int main(int argc, char* argv[])
     // Initialize dynamic memory
     for(int i = 0; i < mug_number; i++)
     {
-        beer_mug[i].picks = 0;
-        pthread_mutex_init(&beer_mug[i].mutex, NULL);
+        coffe_mug[i].picks = 0;
+        pthread_mutex_init(&coffe_mug[i].mutex, NULL);
     }
 
     // Create parallel threads representing clients
@@ -153,18 +152,18 @@ int main(int argc, char* argv[])
         printf("Thread: %d\tWork done: %d\n", i, done_work[i]);
 
     // Output uses of each tap
-    printf("\n---------- Summarization of fills from each tap ---------------\n");
-    for(int i = 0; i < TAP_NUMBER; i++)
+    printf("\n---------- Summarization of fills from each coffe machine ---------------\n");
+    for(int i = 0; i < COFFE_MACHINE_NUMBER; i++)
     {
         // Check if the mutex is unlocked as it should be
         char mutex_state[10] = "Locked";
-        if (pthread_mutex_trylock(&beer_taps[i].mutex) == 0)
+        if (pthread_mutex_trylock(&coffe_machines[i].mutex) == 0)
         {
             strcpy(mutex_state, "Unlocked");
-            pthread_mutex_unlock(&beer_taps[i].mutex);
+            pthread_mutex_unlock(&coffe_machines[i].mutex);
         }
 
-        printf("Tap: %-15s\tFills: %d\tMutex state: %s\n", beer_taps[i].name, beer_taps[i].fills, mutex_state);
+        printf("Tap: %-15s\tFills: %d\tMutex state: %s\n", coffe_machines[i].name, coffe_machines[i].fills, mutex_state);
     }
 
     // Output uses of each mug
@@ -173,13 +172,13 @@ int main(int argc, char* argv[])
     {
         // Check if the mutex is unlocked as it should be
         char mutex_state[10] = "Locked";
-        if (pthread_mutex_trylock(&beer_mug[i].mutex) == 0)
+        if (pthread_mutex_trylock(&coffe_mug[i].mutex) == 0)
         {
             strcpy(mutex_state, "Unlocked");
-            pthread_mutex_unlock(&beer_mug[i].mutex);
+            pthread_mutex_unlock(&coffe_mug[i].mutex);
         }
 
-        printf("Mug: %-15d\tPicks: %d\tMutex state: %s\n", i, beer_mug[i].picks, mutex_state);
+        printf("Mug: %-15d\tPicks: %d\tMutex state: %s\n", i, coffe_mug[i].picks, mutex_state);
     }
 
     printf("\n---------------------- Miscellaneous --------------------------\n");
@@ -188,13 +187,13 @@ int main(int argc, char* argv[])
 
     // Clean mutexes
     for(int i = 0; i < mug_number; i++)
-        pthread_mutex_destroy(&beer_mug[i].mutex);
+        pthread_mutex_destroy(&coffe_mug[i].mutex);
 
-    for(int i = 0; i < TAP_NUMBER; i++)
-        pthread_mutex_destroy(&beer_taps[i].mutex);
+    for(int i = 0; i < COFFE_MACHINE_NUMBER; i++)
+        pthread_mutex_destroy(&coffe_machines[i].mutex);
 
     // Clear dynamically allocated memory
-    free(beer_mug);
+    free(coffe_mug);
     free(threads);
     free(thread_created);
     free(thread_ids);
@@ -218,31 +217,31 @@ void* thread_func(void* arg)
     *done_work = 0;
 
     // Announce the entrance
-    printf("Client %d entered the pub\n", thread_id);
+    printf("Client %d entered the coffe shop\n", thread_id);
 
     while(drinking_limit_left > 0)
     {
         // Try getting a mug
         int my_mug_index = wait_for_a_mug(done_work);
-        printf("Client %d has taken a mug number: %d\n", thread_id, my_mug_index);
+        printf("Client %d has taken a mug number %d\n", thread_id, my_mug_index);
 
         // Try getting to a tap 
-        int used_tap_index = wait_for_a_tap(done_work);
-        printf("Client %d is filling the mug with: %s\n", thread_id, beer_taps[used_tap_index].name);
+        int used_tap_index = wait_for_a_machine(done_work);
+        printf("Client %d is filling the mug with: %s\n", thread_id, coffe_machines[used_tap_index].name);
 
         // Take time to fill the tap
         usleep(MUG_FILL_TIME);
 
         // Leave the tap (unlock the mutex)
-        pthread_mutex_unlock(&beer_taps[used_tap_index].mutex);
-        printf("Client %d has left the tap\n", thread_id);
+        pthread_mutex_unlock(&coffe_machines[used_tap_index].mutex);
+        printf("Client %d has left the machine\n", thread_id);
 
         // Take time to drink the mug contents
-        printf("Client %d is drinking %s from mug %d\n", thread_id, beer_taps[used_tap_index].name, my_mug_index);
+        printf("Client %d is drinking %s from mug %d\n", thread_id, coffe_machines[used_tap_index].name, my_mug_index);
         usleep(MUG_DRINK_TIME);
 
         // Return the mug to the pool (unlock the mutex)
-        pthread_mutex_unlock(&beer_mug[my_mug_index].mutex);
+        pthread_mutex_unlock(&coffe_mug[my_mug_index].mutex);
         printf("Client %d has returned the mug\n", thread_id);
 
         // Decrease drinking limit
@@ -257,10 +256,10 @@ int wait_for_a_mug(int* done_work)
     int i = 0;
     while(true)
     {
-        if(pthread_mutex_trylock(&beer_mug[i].mutex) == 0)
+        if(pthread_mutex_trylock(&coffe_mug[i].mutex) == 0)
         {
             // Reserve the mug
-            beer_mug[i].picks += 1;
+            coffe_mug[i].picks += 1;
 
             // Return the index of the mug
             return i;
@@ -277,15 +276,15 @@ int wait_for_a_mug(int* done_work)
     }
 }
 
-int wait_for_a_tap(int* done_work)
+int wait_for_a_machine(int* done_work)
 {
     int i = 0;
     while(true)
     {
-        if(pthread_mutex_trylock(&beer_taps[i].mutex) == 0)
+        if(pthread_mutex_trylock(&coffe_machines[i].mutex) == 0)
         {
             // Reserve the tap
-            beer_taps[i].fills += 1;
+            coffe_machines[i].fills += 1;
 
             // Return the index of the tap
             return i;
@@ -297,7 +296,7 @@ int wait_for_a_tap(int* done_work)
 
             // Change the index to try another tap
             i += 1;
-            i %= TAP_NUMBER;
+            i %= COFFE_MACHINE_NUMBER;
         }
     }
 }
